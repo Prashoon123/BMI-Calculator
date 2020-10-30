a4d1b4d3092496ef169d84c3d7ba41facfccf02c
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-30 15:13:16 +0530

    added files via upload

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function BMI() {
     var h=document.getElementById('h').value;
     var w=document.getElementById('w').value;
     var bmi=w/(h/100*h/100);
-    var bmio=(bmi.toFixed(2));
-
+    var bmio=(bmi.toFixed(0));
+    if(bmio === "NaN") {
+        alert("Please enter a valid height and weight.")
+    }
     document.getElementById("result").innerHTML="Your BMI is " + bmio;
 }
